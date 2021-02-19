3528aad58e4831fca7d648f650434b5e8708ddac
Vinstagramx <44545069+Vinstagramx@users.noreply.github.com>
2021-02-19 03:55:30 +0000

    PORTD setup
    Setup appears to be successful, with weak pull-up resistors taken into account. Some debugging still needs to be done regarding the variable behaviour.

--- main.s
+++ main.s
@@ -8,14 +8,19 @@ main:
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
+;	movlw 	0x0
+;	movwf	TRISC, A	    ; Port C all outputs
+	movlw	0x1		    ; move 1 into W register
+	movwf	TRISD, A	    ; Set TRISD to all 1s - input
+	movlb	0x0f		    ; RDPU not in access RAM
+	bsf	RDPU		    ; Turn on Port D pull-ups (weak pull-up)
 	bra 	test
 loop:
-	movff 	0x06, PORTC
+;	movff 	0x06, PORTC
+	movff	0x06, PORTD
 	incf 	0x06, W, A
 test:
-	movwf	0x06, A	    ; Test for end of loop condition
+	movwf	0x06, A		    ; Test for end of loop condition
 	movlw 	0x63
 	cpfsgt 	0x06, A
 	bra 	loop		    ; Not yet finished goto start of loop again
